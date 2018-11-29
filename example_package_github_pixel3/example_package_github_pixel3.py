dac70d16c24facda150be4839fd987fb65aa4364
lucidsushi <snowbow2@gmail.com>
2018-11-29 15:19:40 -0500

    Add one test and remove python versions in setup.py

--- example_package_github_pixel3/example_package_github_pixel3.py
+++ example_package_github_pixel3/example_package_github_pixel3.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+def test_message():
+	""" This returns a test message for the unittest """
+	return 'This is a test message for unittest'
--- tests/test_example_package_github_pixel3.py
+++ tests/test_example_package_github_pixel3.py
@@ -16,12 +16,15 @@ class TestExample_package_github_pixel3(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        self.test_message = "This is a test message for unittest"
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
+    def test_test_message(self):
         """Test something."""
+        test_message = example_package_github_pixel3.test_message()
+        self.assertEqual(test_message, self.test_message)
 
     def test_command_line_interface(self):
         """Test the CLI."""
