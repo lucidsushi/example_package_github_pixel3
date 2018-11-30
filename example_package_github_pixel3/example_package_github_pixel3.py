9ba83189f4c5823973b09cc21c31caf54482699a
lucidsushi <snowbow2@gmail.com>
2018-11-30 13:30:07 -0500

    Fix function syntax error

--- example_package_github_pixel3/example_package_github_pixel3.py
+++ example_package_github_pixel3/example_package_github_pixel3.py
@@ -7,6 +7,6 @@ def test_message():
     """ This returns a test message for the unittest """
     return 'This is a test message for unittest'
 
-def multiply_two_number(a, b)
+def multiply_two_number(a, b):
 	""" Returns the result of multiplying two numbers """
 	return a * b
