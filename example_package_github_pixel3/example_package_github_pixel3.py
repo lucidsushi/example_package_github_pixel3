85c0ac0799c43db2643ffaff9147397b1adfbfb1
lucidsushi <snowbow2@gmail.com>
2018-11-30 18:14:46 -0500

    Unimportant refactoring

--- example_package_github_pixel3/example_package_github_pixel3.py
+++ example_package_github_pixel3/example_package_github_pixel3.py
@@ -7,6 +7,7 @@ def test_message():
     """ This returns a test message for the unittest """
     return 'This is a test message for unittest'
 
+
 def multiply_two_number(a, b):
-	""" Returns the result of multiplying two numbers """
-	return a * b
+    """ Returns the result of multiplying two numbers """
+    return a * b
--- tests/test_example_package_github_pixel3.py
+++ tests/test_example_package_github_pixel3.py
@@ -30,7 +30,7 @@ class TestExample_package_github_pixel3(unittest.TestCase):
         """Test the CLI."""
         runner = CliRunner()
         result = runner.invoke(cli.main)
-        assert result.exit_code == 0
+        self.assertEqual(result.exit_code, 0)
         assert 'example_package_github_pixel3.cli.main' in result.output
         help_result = runner.invoke(cli.main, ['--help'])
         assert help_result.exit_code == 0
